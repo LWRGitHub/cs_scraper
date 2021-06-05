ffe64f0139df41406bf9cf2cd43235b53134103a
lwrgithub <loganreynoldslwr@gmail.com>
2021-06-05 16:18:19 -0700

    project ready to show off nolonger privet code

---
+++ README.md
@@ -0,0 +1,68 @@
+# Scrapping Craigslist/Trash Nothing!
+Free Apple products & more through scrapping Craigslist/Trash Nothing! These python files use bs4 & selenium to scrape the web & send you a text message when you find that awesome thing your looking for! The file are setup to for searching for free apple products, computers, laptops, & video games. 
+
+***The story behind this build***
+My Friend was telling me how he got a free Mac Book Air from Craigslist! I was like wow, & I started looking at Craigslist through out the day. But I was not finding anything, although my friend keep finding tons of free apple product one he sold for over $2K! So I build a web scraper & the rest is history!
+
+## Tech Used
+- Python
+- Bs4
+- Selenium
+- requests
+- dotenv
+
+
+## Setup, Install & Run
+
+1. `pip3 install -r requirements.txt`
+2. Creat a free account on [TextFree.us](https://messages.textfree.us/login)
+3. [Download Chrome Driver](https://sites.google.com/achromium.org/chromedriver/downloads)
+    - NOTE: You have Google Chrome & the Chrome Driver must be the same vertion
+    - FIND VERTION: 
+        - Open Google Chrome
+        - click 3 dots in upper right corner,
+        - click `Help`
+        - click `About Google Chrome`
+4. `mv env.example .env`
+5. Setup .env
+    - Text Free Username & Password  
+    ```
+    TEXT_FREE_USERNAME=UserName
+    TEXT_FREE_PASS=Password
+    ```
+    - phone number you can be texted at
+    `MSM_NUM=5555555555`
+    - your zip code
+    `CRAIGSLIST_ZIP=55555`
+    - find & copy the path where you saved chromedriver
+    `CHROME_DRIVER_PATH=/some/path/to/chromedriver`
+    - search your zip code on trashnothing.com & copy the radiaous in the URL
+    `TRASH_NOTHING_RADIAOUS=555555`
+6. choice file to run
+    - `python3 apple_craigslitst_scrape.py`
+    - `python3 apple_trash_nothing.py`
+    - `python3 computer_craigslit.py`
+    - `python3 computer_trash_nothing.py`
+    - `python3 laptop_craigslit.py`
+    - `python3 laptop_trash_nothing.py`
+    - `python3 mac_craiglist.py`
+    - `python3 mac_trash_nothing.py`
+    - `python3 video_game_craigslist.py`
+    - `python3 video_game_trash_nothing.py`
+
+
+## IMG
+
+***Enter command***
+
+![Image of the terminal just before entering the command to run the scraper](http://raw.githubusercontent.com/LWRGitHub/cs_scraper/master/img/start.png)
+
+
+***Runing***
+
+![Image of the terminal runing the web scraper](http://raw.githubusercontent.com/LWRGitHub/cs_scraper/master/img/runing.png)
+
+
+***App sending you a Text***
+
+![Image of the app having found something & starting to text a number.](http://raw.githubusercontent.com/LWRGitHub/cs_scraper/master/img/text.png)
---
+++ laptop_trash_nothing.py
@@ -0,0 +1,124 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from selenium import webdriver
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TEXT_FREE_USERNAME = os.getenv('TEXT_FREE_USERNAME')
+TEXT_FREE_PASS = os.getenv('TEXT_FREE_PASS')
+MSM_NUM = os.getenv('MSM_NUM')
+CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
+RADIAOUS = os.getenv('TRASH_NOTHING_RADIAOUS')
+
+# set old data
+r = requests.get(f"https://trashnothing.com/beta/browse?search=laptop&radius={RADIAOUS}")
+soup = bs(r.content)
+old_data = soup.find_all("a", attrs={"class": "offer"})
+
+# uncomment `old_data` to recive all items when you first run the app
+# old_data = []
+
+all_herf = []
+for a in old_data:
+    all_herf.append(a['href'])
+print(all_herf)
+
+while True:
+    # get new data
+    r = requests.get(f"https://trashnothing.com/beta/browse?search=laptop&radius={RADIAOUS}")
+    soup = bs(r.content)
+    recent_data = soup.find_all("a", attrs={"class": "offer"})
+
+    if old_data != recent_data:
+        new_href = []
+
+        print("--------------------- recent_data ------------------ ")
+        print(recent_data)
+        print("--------------------- /recent_data ------------------ ")
+
+        for a in recent_data:
+            print("------------- a -----------")
+            print(a['href'])
+            print("------------- /a -----------")
+            new_href.append(a['href'])
+
+        print("--------------------- Compair old & new ------------------ ")
+        print("--------------------- old ------------------ ")
+        print(recent_data)
+        print("--------------------- /old ------------------ ")
+        print("--------------------- new ------------------ ")
+        print(old_data)
+        print("--------------------- /new ------------------ ")
+        print("--------------------- /Compair old & new ------------------ ")
+
+        there_is_a_new_href = False
+
+        message_to_send = ''
+        for data_href in new_href:
+            if data_href not in all_herf:
+                there_is_a_new_href = True
+                message_to_send += f"https://trashnothing.com{data_href} "
+                all_herf.append(data_href)
+
+        
+        if there_is_a_new_href:
+            # new_a = []
+            
+            print("----------------message----------------")
+            print(message_to_send)
+            print("----------------/message----------------")
+
+            old_data = recent_data
+
+            driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+
+            driver.get("https://messages.textfree.us/login")
+
+            time.sleep(2)
+            # username
+            username = driver.find_element_by_name("username")
+            username.send_keys(TEXT_FREE_USERNAME)
+
+            # password
+            password = driver.find_element_by_name("password")
+            password.send_keys(TEXT_FREE_PASS)
+
+            #log in btn
+            log_in_btn = driver.find_element_by_class_name('button-purple')
+            log_in_btn.click()
+
+            time.sleep(2)
+            #close pop up
+            SyncContactsXDismissPopup = driver.find_element_by_id('SyncContactsXDismissPopup')
+            SyncContactsXDismissPopup.click()
+
+            # selevxt startNewConversationButton
+            startNewConversationButton = driver.find_element_by_id('startNewConversationButton')
+            startNewConversationButton.click()
+
+            #number input
+            contactInput = driver.find_element_by_id('contactInput')
+            contactInput.send_keys(MSM_NUM)
+
+            #click to input message
+            emojionearea_editor = driver.find_element_by_class_name('emojionearea-editor')
+
+            if message_to_send != '':
+                emojionearea_editor.send_keys(f"https://trashnothing.com/beta/browse?search=laptop&radius={RADIAOUS} Check Trash Nothing 'Laptop' stuff! {message_to_send}")
+            else:
+                emojionearea_editor.send_keys(f'Something is wrong with Trash Nothing "Laptop" bot https://trashnothing.com/beta/browse?search=laptop&radius={RADIAOUS}')
+
+            #send
+            sendButton = driver.find_element_by_id('sendButton')
+            sendButton.click()
+
+            time.sleep(2)
+            driver.quit()
+        else:
+            old_data = recent_data
+ 
+
+    time.sleep(2)
